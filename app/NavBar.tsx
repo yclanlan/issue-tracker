bbd7e49925903ceb3e23809fa844a9a5af400422
yclanlan <lanlan.yichun@gmail.com>
2024-03-24 15:08:15 -0400

    build a navbar

---
+++ app/NavBar.tsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import Link from 'next/link'
+import { IoMdBug } from "react-icons/io";
+
+const NavBar = () => {
+
+const links=[
+    {label:'Dashboard',href:'/'},
+    {label:'Issues',href:'/issues'},    
+
+]
+
+  return (
+    <nav className='flex space-x-6 border-b mb-5 px-5 h-14 items-center'>
+        <Link href="/">  <IoMdBug /> </Link>
+        <ul className='flex space-x-6'>
+            {links.map(link => 
+            <Link key={link.href} 
+            className='text-zinc-500 hover:text-zinc-900 transition-colors' 
+            href={link.href}> {link.label} </Link>)}
+            
+        </ul>
+
+    </nav>
+  )
+}
+
+export default NavBar
